d56e19e32377c00ef4c7526598b4f6835433af0c
fu-jw <contact@fu-jw.com>
2024-01-02 10:09:09 +0800

    update：修改路径

--- 03.react_component/src/App.jsx
+++ 03.react_component/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import HelloWorld from "./Components/HellWorld"
-import HelloReact from "./Components/HelloReact"
+import HelloWorld from "./Components/00.hello/HellWorld"
+import HelloReact from "./Components/00.hello/HelloReact"
 
 // 编写一个组件
 class App extends React.Component {
