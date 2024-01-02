e75e041d59759a63eb849f32c346da1cea690611
fu-jw <contact@fu-jw.com>
2024-01-02 14:28:35 +0800

    update：组件生命周期 end

--- 03.react_component/src/Components/01.生命周期/HelloWorld.jsx
+++ 03.react_component/src/Components/01.生命周期/HelloWorld.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+// 生命周期演示，https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
 class HelloWorld extends React.Component {
   // 1.构造方法: constructor
   constructor() {
@@ -45,12 +45,14 @@ class HelloWorld extends React.Component {
 
 
   // 不常用的生命周期补充
+  // 决定组件是否重新渲染，返回值为true/false。默认返回true
   shouldComponentUpdate() {
     return true
   }
-
+  // 组件将要被更新, 在render函数之前执行, 返回值会作为render函数的第三个参数, 一般用于保存滚动位置, 以便在componentDidUpdate中使用, 也可以返回null, 表示不保存任何值 
   getSnapshotBeforeUpdate() {
     console.log("getSnapshotBeforeUpdate")
+    // 这里返回的值会作为componentDidUpdate的第三个参数
     return {
       scrollPosition: 1000
     }
