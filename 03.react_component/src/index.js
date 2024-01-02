2fe4b67c08e798c09ad8a14654cdc38e07778b40
fu-jw <contact@fu-jw.com>
2024-01-02 16:05:36 +0800

    add:组件通信 init

--- 03.react_component/src/index.js
+++ 03.react_component/src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client"
 
 // import App from "./Components/00.hello/App"
-import App from "./Components/01.生命周期/App"
+// import App from "./Components/01.生命周期/App"
+import App from "./Components/02.组件通信/App"
 
 // 编写React代码, 并且通过React渲染出来对应的内容
 const root = ReactDOM.createRoot(document.querySelector("#root"))
